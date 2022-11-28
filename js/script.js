3385ef46dc93ae37c31e6735e1616897118dc0b9
Alexbarile <alexbarile93fg@yahoo.it>
2022-11-28 15:35:01 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// 1 - chiedere il numero dei km
+
+let km = parseInt(prompt ('numero dei km'));
+
+
+// 2 - chiedere l’età
+
+let eta = parseInt(prompt ('numero età'));
+
+// 3 - calcolo il prezzo
+
+let price = km * 0.21;
+
+// SE / ELSE 
+
+if(eta < 18){
+    // prezzo (price) - il 20%
+    price = price - ((price/100) * 20);
+    
+}else if(eta > 65){
+    // prezzo (price) - il 40%
+    price = price - ((price/100) * 40);
+
+}
+
+else{
+
+}
+
+// 4 - Mostro il prezzo in un formato leggibile (massimo due punti decimali)
+
+document.getElementById('prezzo').innerHTML = 'il tuo prezzo è' + " " + price.toFixed(2);
---
+++ readme.txt
@@ -0,0 +1,13 @@
+PROBLEMA: calcolo del prezzo del biglietto del treno
+
+1 - chiedere il numero dei km
+2 - chiedere l’età
+3 - calcolo il prezzo
+  SE età < 18 
+    applico uno sconto del 20%
+  ALTRIMENTI se età > 65 
+    applico uno sconto del 40%
+  ALTRIMENTI 
+    non applico nessuno sconto
+4 - Mostro il prezzo in un formato leggibile
+    4.1 massimo due punti decimali
